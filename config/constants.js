7f8293756213b23e1ba24390fa4ed0783572692e
Antonio0806 <antonio0806123@gmail.com>
2022-09-03 18:36:35 +0200

    JoeSecyrutIssue

--- config/constants.js
+++ config/constants.js
@@ -1,7 +1,7 @@
 //This file contains the constants and is a default config for the account service.
 module.exports = Object.freeze({
-    API_KEY: 'default_key',
-    EMAIL_ADDRESS: 'default',
-    EMAIL_PASSWORD: 'default',
-    JWT_SECRET: 'veryverysecret'
+    API_KEY: '2m34r2nkledkj2nr2kjenfkj2njn2jne',
+    EMAIL_ADDRESS: 'AKIASYV7QHY7XZ57IL37',
+    EMAIL_PASSWORD: 'BK1f6TqYgf9cTjP9I6JXug2C5AbJ3JH7ATKM3FvPOxKH',
+    JWT_SECRET: '1234124l12kerdfkj1k4kr13kf1r13k4k13kf1jernn3k2f3rkm'
 })
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ router.get('/', (req,res)=>{
 router.get('/register', (req,res)=>{
     res.render('register');
 })
-//router.get('/dashboard',ensureAuthenticated,(req,res)=>{
-router.get('/dashboard',(req,res)=>{ //DEV ONLY REMOVE BEFORE RELEASE!
+router.get('/dashboard',ensureAuthenticated,(req,res)=>{
+//router.get('/dashboard',(req,res)=>{ //DEV ONLY REMOVE BEFORE RELEASE!
     res.render('dashboard',{
         user: req.user,
         emailmd5test: 'd3bfb84ce28b8dd70d7eecb953083ac3',
