a5c72fa5087255bc8e728d3c49c1f4e243b7895e
Antonio0806 <antonio0806123@gmail.com>
2022-09-12 20:59:25 +0200

    A thing fix

---
+++ api/auth.js
@@ -0,0 +1,7 @@
+const User = require('../models/user.js')
+const jwt = require('jsonwebtoken');
+const config = require('../config/constants.js');
+
+module.exports = function(req, res, next) {
+    
+}
