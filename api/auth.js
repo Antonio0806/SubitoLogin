b46dd220b9a64127d68765554452593c2bf8303a
Antonio0806 <antonio0806123@gmail.com>
2022-12-11 11:29:47 +0100

    Added tokens for applications logging in.

--- api/auth.js
+++ api/auth.js
@@ -11,11 +11,17 @@ module.exports = function(req, res, next) {
                  if(err) throw err;
                 User.findOne({email : req.params.email}).exec((err,user)=>{
                     if(req.params.password == user.passwordC) {
+                        var tokensigned = jwt.sign({
+                            data: Buffer.from(Date.now().toString()).toString('base64')
+                            }, config.JWT_SECRET 
+                        );    
                         var response = {
                             message: 1,
-                            token: user.emailmd5,
+                            token: tokensigned,
                             emailmd5: user.emailmd5,
-                            name: user.name  
+                            name: user.name,
+                            date: user.date,
+                            isVerified: user.isVerified  
                         };
                         res.send(JSON.stringify(response));
                     } else {
@@ -23,7 +29,8 @@ module.exports = function(req, res, next) {
                             message: 2,
                             token: 0,
                             emailmd5: 0,
-                            name: 0  
+                            name: 0, 
+                            date: 0 
                         };
                         res.send(JSON.stringify(response));
                     }
