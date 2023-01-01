0ee6fd24ee9f9f6eb24384cb91d37354f7cde6ce
Antonio0806 <antonio0806123@gmail.com>
2023-01-01 22:59:25 +0100

    Added CORS support

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ app.use((req,res,next)=> {
     res.locals.error  = req.flash('error');
     next();
     })
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
     
 //Routes
 app.use('/', require('./routes/index'));
